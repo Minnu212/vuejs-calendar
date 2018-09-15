7073c250c5f6c29babb77ef57694f0663cf64a89
Minhajuddin Quazi <quazi.minhaj88@gmail.com>
2018-09-16 02:35:44 +0530

    First Commit

---
+++ src/components/App.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <div id="day-bar">
+            <div>MON</div>
+            <div>TUE</div>
+            <div>WED</div>
+            <div>THUR</div>
+            <div>FRI</div>
+            <div>SAT</div>
+            <div>SUN</div>
+        </div>
+        <div id="calendar">
+            <div class="calendar-week" v-for="(week,wi) in weeks" :key="wi">
+                <calendar-day v-for="(day,di) in week" :key="di" :day="day">
+                </calendar-day>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+import CalendarDay from './CalendarDay.vue'
+export default {
+  data() {
+    return {
+      month: 9,
+      year: 2018
+    };
+  },
+  computed: {
+    days() {
+      let days = [];
+      let SUNDAY = 0;
+      let MONDAY = 1;
+      let currentDay = this.$moment(`${this.year}-${this.month}-1`, `YYYY-M-D`);
+      // generating all days in current month
+      do {
+        days.push(currentDay);
+        currentDay = this.$moment(currentDay).add(1, "days");
+      } while (currentDay.month() + 1 === this.month);
+
+      // generating previous month days
+      currentDay = days[0];
+      if (currentDay.day() !== MONDAY) {
+        do {
+          currentDay = this.$moment(currentDay).add(-1, "days");
+          days.unshift(currentDay);
+        } while (currentDay.day() !== MONDAY);
+      }
+
+      //generating next month days
+      currentDay = days[days.length - 1];
+      if (currentDay.day() !== SUNDAY) {
+        do {
+          currentDay = this.$moment(currentDay).add(1, "days");
+          days.push(currentDay);
+        } while (currentDay.day() !== SUNDAY);
+      }
+
+      return days;
+    },
+    weeks() {
+      let weeks = [];
+      let week = [];
+
+      for (let day of this.days) {
+        week.push(day);
+        if (week.length === 7) {
+          weeks.push(week);
+          week = [];
+        }
+      }
+      return weeks;
+    }
+  },
+  components:{
+      CalendarDay
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :class="classObject">
+        {{day.format('D')}}
+    </div>
+</template>
+
+<script>
+export default {
+  props: ["day"],
+  computed: {
+    classObject() {
+      return {
+        day: true,
+        today: this.day.isSame(this.$moment(), "day")
+      };
+    }
+  },
+  created(){
+      console.log('cc')
+      console.log(this.$moment())
+  }
+};
+</script>
+
+<style>
+</style>
